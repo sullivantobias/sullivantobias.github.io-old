eb327c1b64f997890b7f8bd97666bb31fcdbf0f0
sullivan <sullytobias@gmail.com>
2020-05-04 18:43:52 +0200

    Init Real Portfolio

---
+++ precache-manifest.691a7d5d1c51ac16690d5eb6f58ecdae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b54e930415c14f55e857b5e836d7a221",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc97313ae1c25017a8db",
+    "url": "/static/css/main.d6b9d246.chunk.css"
+  },
+  {
+    "revision": "04626afef67cee9483b7",
+    "url": "/static/js/2.093e4870.chunk.js"
+  },
+  {
+    "revision": "8284086be66d015d06c799a0f515853c",
+    "url": "/static/js/2.093e4870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc97313ae1c25017a8db",
+    "url": "/static/js/main.b76f7c72.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "3bfe990d36d5ffae342b20b5d768d9ce",
+    "url": "/static/media/avatar.3bfe990d.png"
+  }
+]);
