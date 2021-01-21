7839b9ae44265a5dbf8a7d2296ebb3d9dcde6134
sullivan <stobias@sqli.com>
2021-01-21 18:57:03 +0100

    Minor Changes

---
+++ precache-manifest.f6218d5d3a5b2393070fc28d1748ee74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c759fdaa1fc701f18e6ae256e51727c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a3abb8763d7ce2bdd48",
+    "url": "/static/css/main.8d73fd66.chunk.css"
+  },
+  {
+    "revision": "1123a5eb6369ceeac042",
+    "url": "/static/js/2.c86514df.chunk.js"
+  },
+  {
+    "revision": "8284086be66d015d06c799a0f515853c",
+    "url": "/static/js/2.c86514df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a3abb8763d7ce2bdd48",
+    "url": "/static/js/main.3b87ff69.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "3bfe990d36d5ffae342b20b5d768d9ce",
+    "url": "/static/media/avatar.3bfe990d.png"
+  }
+]);
