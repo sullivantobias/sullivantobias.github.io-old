0d3698e79a64cf62c1084c7684214561f6bf3394
sullivan <sullytobias@gmail.com>
2021-01-23 23:09:00 +0100

    Release 1.6.0

---
+++ precache-manifest.cf6775bc2b4fcaad1c1bd62578e68bff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502a75e3a39b7752903f3d6d02ee6a77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7b627a78f8f835ad24a",
+    "url": "/static/css/main.6657fc99.chunk.css"
+  },
+  {
+    "revision": "1123a5eb6369ceeac042",
+    "url": "/static/js/2.c86514df.chunk.js"
+  },
+  {
+    "revision": "8284086be66d015d06c799a0f515853c",
+    "url": "/static/js/2.c86514df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7b627a78f8f835ad24a",
+    "url": "/static/js/main.9ff092b2.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "3bfe990d36d5ffae342b20b5d768d9ce",
+    "url": "/static/media/avatar.3bfe990d.png"
+  }
+]);
